db247f808870cabe539d8494d634c3b1e4940676
shamiul <anik.savar.bd@gmail.com>
2023-04-30 09:24:39 +0600

    Dynamic link added

--- index.js
+++ index.js
@@ -20,6 +20,23 @@ app.get("/news", (req, res) => {
   res.send(news); // sending news as response
 });
 
+app.get(`/news/:id`, (req, res) => {
+  const id = req.params.id; // requesting for specific news with id
+  const getNews = news.find((n) => n._id === id);
+  res.send(getNews); // finding news with id
+});
+
+app.get(`/category/:id`, (req, res) => {
+  const id = req.params.id;
+  if (id === "0") {
+    res.send(news); // sending all news
+  }
+  else {
+    const getNewsWithCategory = news.filter((n) => n.category_id === id);
+    res.send(getNewsWithCategory); // finding news with id for specific category
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Dragon API is listening on port ${port}`);
+  console.log(`Listening on port: ${port}`);
 });
